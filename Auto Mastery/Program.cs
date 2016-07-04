30550111529984a71c28b280f351dac3966b6cc2
Speedy50 <iplayblitzcrankonly@gmail.com>
2016-07-04 15:01:30 +0200

    Release

---
+++ Auto Mastery/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+
+namespace AutoMastery
+{
+    class Program
+    {
+        static void Main(string[] args) { Loading.OnLoadingComplete += Loading_OnLoadingComplete; }
+        private static AIHeroClient myhero { get { return ObjectManager.Player; } }
+        private static int CurrentKills, TotalKills;
+        private static bool UsedEmote;
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Chat.Print("<font color='#0080FF'>Speedy50 (Draven)</font>: Auto Mastery Loaded! (v1.0)");
+            Chat.Print("by Speedy50");
+            Game.OnUpdate += OnUpdate;
+            TotalKills = 0;
+        }
+        private static void OnUpdate(EventArgs args)
+        {
+            if (myhero.IsDead) return;
+
+            CurrentKills = myhero.ChampionsKilled;
+
+            if (CurrentKills == TotalKills)
+            {
+                UsedEmote = false;
+            }
+
+            if (CurrentKills > TotalKills && UsedEmote == false)
+            {
+                Chat.Say("/masterybadge");
+                UsedEmote = true;
+                TotalKills = CurrentKills;
+            }
+        }
+    }
+}
---
+++ Auto Mastery/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Auto Mastery")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Auto Mastery")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("ac810b3e-e297-432c-a056-decdee2e5504")]
+
