591f5e4d2978e3f61f5c210048be43af02f0c77f
Speedy50 <iplayblitzcrankonly@gmail.com>
2016-08-07 12:06:41 +0200

    -

--- LWolf Mode/Properties/AssemblyInfo.cs
+++ LWolf Mode/Properties/AssemblyInfo.cs
@@ -3,11 +3,11 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyTitle("Speedy50 Mode")]
+[assembly: AssemblyTitle("LWolf Mode")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Speedy50 Mode")]
+[assembly: AssemblyProduct("LWolf Mode")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
